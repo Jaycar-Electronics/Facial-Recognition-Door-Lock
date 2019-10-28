fe5e75175eb65d5a91d5db8cb04274eea8530468
Jaycar-Electronics <43460991+Jaycar-Electronics@users.noreply.github.com>
2019-10-29 10:46:58 +1100

    removed images and useless scripts

--- other_scripts/localtest.py
+++
@@ -1,7 +0,0 @@
-import asyncio
-from functions import videoProcessing
-from identifier import Identifier
-
-i = Identifier()
-
-asyncio.ensure_future(videoProcessing(i,True))
--- setup.sh
+++ setup.sh
@@ -1,18 +1,28 @@
+
+#set up the system, update packages, install pip and needed tools
 sudo apt-get update
 sudo apt-get upgrade -y
+sudo apt-get install python3-pip -y
+#install python packages
 sudo pip3 install opencv-python sanic asyncio numpy json
 
+#set up the swap-size then install the face-recog package, which will compile.
 sudo sed -i 's/SWAPSIZE=.*$/SWAPSIZE=1024/g' /etc/dphys-swapfile
 sudo /etc/init.d/dphys-swapfile restart
-
 sudo pip3 install face-recognition
 
+# TODO: auto-install python scripts into /usr/bin
+# connect up to the init.d/ as proper
+# make it behave like a proper program 
+
+# install the doorlock program starter in /etc/init.d/
 sudo cp misc/doorlock.sh /etc/init.d/doorlock
 sudo chmod +x /etc/init.d/doorlock
 
 # this should hopefully start up the program on boot.
 sudo update-rc.d doorlock defaults
 
+# output a "helpful" message 
 echo "###############################"
 echo "remember: "
 echo " sudo /etc/inid.d/doorlock start"
@@ -27,5 +37,5 @@ echo "https://github.com/Jaycar-Electronics/Facial-Recognition-Door-Lock"
 echo "###############################"
 echo " Setup done, reboot or start the command as above."
 echo " Your ip is:"
-# is there a better way to do this? yes. do I care? not until I've had coffee
+
 ifconfig | grep 'inet ' | grep -v '127.0.0.1' | awk '{print $2}'
